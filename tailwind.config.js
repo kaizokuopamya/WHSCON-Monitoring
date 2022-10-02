470f983a38220fff25f62a382a5a9df327e91189
Powar <pramod-balkrushna.powar@capgemini.com>
2022-10-02 19:53:58 +0530

    added purge to tailwind config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  purge: [
+    "./src/**/*.{js,jsx}",
+    "./public/index.html",
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+  darkMode: "class",
+};
